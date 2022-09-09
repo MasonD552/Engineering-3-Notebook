fcf417601c43221fdb4b702f480aa16fddbed2a7
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-09 09:52:24 -0400

    Hello CircuitPython

--- code.py
+++ code.py
@@ -1,5 +1,10 @@
-from time import sleep
+import board
+import neopixel
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.5 
+
+print("Make it red!")
 
 while True:
-    print("Hello world!")
-    sleep(2)
+    dot.fill((0, 0, 255))

f09e230698a87c202580586c49843a660ef082c0
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-12 09:57:08 -0400

    blinking led

--- firstRgb.py
+++ firstRgb.py
@@ -1,5 +1,7 @@
+from asyncore import loop
 import board
 import neopixel
+import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.2 
@@ -8,3 +10,6 @@ print("Make it red!")
 
 while True:
     dot.fill((255, 0, 0))
+    time.sleep(0.1)
+    dot.fill((0, 0, 0))
+    time.sleep(0.1)

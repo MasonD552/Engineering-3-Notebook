5cabd70941e28aa2c89d48948d84e57b4fc3df95
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-12 09:40:43 -0400

    changed led color to red dot brightness to 0.2

--- firstRgb.py
+++ firstRgb.py
@@ -2,9 +2,9 @@ import board
 import neopixel
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.1 
+dot.brightness = 0.2 
 
 print("Make it red!")
 
 while True:
-    dot.fill((0, 255, 255))
+    dot.fill((255, 0, 0))

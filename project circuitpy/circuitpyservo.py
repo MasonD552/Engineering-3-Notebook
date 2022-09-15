8d46eb47928c939ba634758550a3f40e4c4fcc29
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-15 09:22:50 -0400

    5 degrees->1

--- project circuitpy/circuitpyservo.py
+++ project circuitpy/circuitpyservo.py
@@ -15,10 +15,10 @@ pwm = pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 1):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(0, 180, 1):  # 0 - 180 degrees, 1 degrees at a time.
         my_servo.angle = angle
         time.sleep(0.05)
-    for angle in range(180, 0, -1): # 180 - 0 degrees, 5 degrees at a time.
+    for angle in range(180, 0, -1): # 180 - 0 degrees, 1 degrees at a time.
         my_servo.angle = angle
         time.sleep(0.05)
 #lol

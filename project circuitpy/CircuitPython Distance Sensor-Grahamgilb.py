fb8f32bccfd7ec9840c50f609d8db31f7be36bb9
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-19 09:12:44 -0400

    distnace fade, library update

---
+++ project circuitpy/CircuitPython Distance Sensor-Grahamgilb.py
@@ -0,0 +1,23 @@
+import time
+import board
+import adafruit_hcsr04
+import neopixel
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
+Grant = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
+Grant.brightness = .3  #setting the brightness of the light, from 0-1 brightness
+
+while True:
+    try:
+        cm = sonar.distance
+        print((sonar.distance))
+        time.sleep(0.5)
+        if cm < 5:
+            Grant.fill((255, 0, 0))#setting the color with RGB values
+        if cm > 5 and cm < 20:
+            Grant.fill((255, 255, 0))#setting the color with RGB values
+        if cm > 20:
+            Grant.fill((0, 255, 0))#setting the color with RGB values
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)

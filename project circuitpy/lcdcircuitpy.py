db7a7b03ceee014c3b8bbb80da79cf6f790d890b
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-27 09:11:56 -0400

    LCD and settings

--- project circuitpy/lcdcircuitpy.py
+++ project circuitpy/lcdcircuitpy.py
@@ -1,11 +1,50 @@
+#Code from Graham G-S
 import board
+import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from digitalio import DigitalInOut, Direction, Pull
 
 # get and i2c object
 i2c = board.I2C()
-
+btn = DigitalInOut(board.D3)
+btn2 = DigitalInOut(board.D2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.UP
 # some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
+cur_state = True
+prev_state = True
+cur_state2 = True
+prev_state2 = True
+buttonPress = 0
 
-lcd.print("Hello, Engineer!")
+while True:
+    while btn2.value == False:
+        cur_state = btn.value
+        if cur_state != prev_state:
+            if not cur_state:
+                buttonPress = buttonPress + 1
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+        prev_state = cur_state
+    else:
+        cur_state2 = btn.value
+        if cur_state2 != prev_state2:
+            if not cur_state2:
+                buttonPress = buttonPress - 1
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+        prev_state2 = cur_state2

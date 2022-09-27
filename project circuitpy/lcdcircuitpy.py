94b563b6f331741b5c650c4ffe93fa40c8cbfee7
MasonD552 <mdivers67@charlottesvilleschools.org>
2022-09-27 10:00:25 -0400

    LCDbutton

--- project circuitpy/lcdcircuitpy.py
+++ project circuitpy/lcdcircuitpy.py
@@ -1,50 +1,33 @@
-#Code from Graham G-S
+#Mason Divers
+#LCD counter counts up when one button is held and the other buttton is pressed.
+#Thank you Kaz Shinozaki for your beautiful code.
 import board
+import math
 import time
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from lcd.lcd import LCD                                     #[4-14] code to connect 
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface   #input pins to board
 from digitalio import DigitalInOut, Direction, Pull
-
-# get and i2c object
 i2c = board.I2C()
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
 btn = DigitalInOut(board.D3)
 btn2 = DigitalInOut(board.D2)
 btn.direction = Direction.INPUT
-btn.pull = Pull.UP
 btn2.direction = Direction.INPUT
+btn.pull = Pull.UP
 btn2.pull = Pull.UP
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
-cur_state = True
-prev_state = True
-cur_state2 = True
-prev_state2 = True
-buttonPress = 0
+num = 0                         #Display Variable
+Redo = True                     #[16-17] Variable to "debounce" button
 
-while True:
-    while btn2.value == False:
-        cur_state = btn.value
-        if cur_state != prev_state:
-            if not cur_state:
-                buttonPress = buttonPress + 1
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-            else:
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-        prev_state = cur_state
-    else:
-        cur_state2 = btn.value
-        if cur_state2 != prev_state2:
-            if not cur_state2:
-                buttonPress = buttonPress - 1
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-            else:
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-        prev_state2 = cur_state2
+lcd.print("Starting")
+while True:                                 #[19-30] Code to add and subtract 
+    if btn.value == True and Redo == True:  #from variable and 
+        if btn2.value == True:              #"debounce" the  #buttons.         
+            num = num - 1
+        else:
+            num = num + 1                                   
+        lcd.clear()
+        lcd.print(str(num))
+        Redo = False
+        time.sleep(.1)
+    elif btn.value == False and Redo == False:
+        Redo = True

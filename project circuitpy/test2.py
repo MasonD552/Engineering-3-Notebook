c8f1ebc6d7c5bd99f2345d56458149cd10335b98
MasonD552 <mdivers67@charlottesvilleschools.org>
2023-03-06 09:57:38 -0500

    update

--- project circuitpy/test2.py
+++ project circuitpy/test2.py
@@ -1,4 +1,3 @@
-import time
 from time import sleep
 import board
 from digitalio import DigitalInOut, Direction, Pull
@@ -6,18 +5,13 @@ from pwmio import PWMOut
 from adafruit_motor import motor as Motor
 
 DEBUG = True  # mode of operation; False = normal, True = debug
-OP_DURATION = 1  # operation duration in seconds
+OP_DURATION = 5  # operation duration in seconds
 
 drv6612_ain1 = PWMOut(board.D5, frequency=50)
 drv6612_ain2 = PWMOut(board.D4, frequency=50)
 drv6612_sleep = DigitalInOut(board.D0)
 
-button_a = DigitalInOut(board.D8)
-button_a.direction = Direction.INPUT
-button_a.pull = Pull.DOWN
-
 motor_a = Motor.DCMotor(drv6612_ain1, drv6612_ain2)
-
 
 def print_motor_status(motor):
     if motor == motor_a:
@@ -25,21 +19,32 @@ def print_motor_status(motor):
     else:
         motor_name = "Unknown"
     print(f"Motor {motor_name} throttle is set to {motor.throttle}.")
+
+def basic_operations():
+    # Drive forward at full throttle
+    motor_a.throttle = 1.0
+    if DEBUG: print_motor_status(motor_a)
+    sleep(OP_DURATION)
+
+    # Coast to a stop
+    motor_a.throttle = None
+    if DEBUG: print_motor_status(motor_a)
+    sleep(OP_DURATION)
+
+    # Drive backwards at 50% throttle
+    motor_a.throttle = -0.5
+    if DEBUG: print_motor_status(motor_a)
+    sleep(OP_DURATION)
+
+    # Brake to a stop
+    motor_a.throttle = 0
+    if DEBUG: print_motor_status(motor_a)
+    sleep(OP_DURATION)
+
 # Main
 drv6612_sleep.direction = Direction.OUTPUT
 drv6612_sleep.value = True  # enable (turn on) the motor driver
 
+if DEBUG: print("Running in DEBUG mode.  Turn off for normal operation.")
 while True:
-    if button_a.value:
-        print((1,))
-        def basic_operations():
-            # Drive forward at full throttle
-            motor_a.throttle = 1.0
-            time.sleep(0.5)
-
-    else:
-        print((0,))
-    time.sleep(0.1)
-    def basic_operations():
-        # Brake to a stop
-        motor_a.throttle = None
+    basic_operations()  # perform basic motor control operations on motor A

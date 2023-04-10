16bce9422f113da845dd704f5a69685b3f53b14d
MasonD552 <mdivers67@charlottesvilleschools.org>
2023-04-10 09:14:17 -0400

    changes to baseball throwing robot

--- project circuitpy/Baseball_throwing_robot.py
+++ project circuitpy/Baseball_throwing_robot.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import time
 from time import sleep
 import board
@@ -5,10 +6,12 @@ from digitalio import DigitalInOut, Direction, Pull
 from pwmio import PWMOut
 from adafruit_motor import motor as Motor
 
+# Set up DRV6612 motor driver inputs and outputs
 drv6612_ain1 = PWMOut(board.D5, frequency=50)
 drv6612_ain2 = PWMOut(board.D4, frequency=50)
 drv6612_sleep = DigitalInOut(board.D0)
 
+# Set up button and switch inputs
 button_a = DigitalInOut(board.D13)
 button_a.direction = Direction.INPUT
 button_a.pull = Pull.DOWN
@@ -16,57 +19,58 @@ switch = DigitalInOut(board.D10)
 switch.direction = Direction.INPUT
 switch.pull = Pull.UP
 
+# Set up motor using DRV6612 inputs
 motor_a = Motor.DCMotor(drv6612_ain1, drv6612_ain2)
 timeStop = 0.0
 currentTime = time.monotonic()
 
+# Define function to print motor status
+
 
 def print_motor_status(motor):
-    if motor == motor_a:
-        motor_name = "A"
-    else:
-        motor_name = "Unknown"
-    print(f"Motor {motor_name} throttle is set to {motor.throttle}.")
 
 
-# Main
+if (motor == motor_a):
+motor_name = "A"
+else:
+motor_name = "Unknown"
+print(f"Motor {motor_name} throttle is set to {motor.throttle}.")
+
+# Main loop
 drv6612_sleep.direction = Direction.OUTPUT
 drv6612_sleep.value = True  # enable (turn on) the motor driver
-
 while True:
+    # If button and switch are both pressed
+if (button_a.value == 1 & switch.value == 1):
+    # Drive forward at full throttle for 5 seconds
+motor_a.throttle = 1.0
+timeStop = (time.monotonic() + 5)
 
-    if (button_a.value == 1 & switch.value == 1):
-        #print(('Button Pressed & Switch On',))
-        # Drive forward at full throttle
-        motor_a.throttle = 1.0
-        timeStop = (time.monotonic() + 5)
-    
-    if (switch.value == 0):
-        #print(('Switch Off',))
-        # Brake to a stop
-        motor_a.throttle = None
+# If switch is turned off
+if (switch.value == 0):
+    # Brake to a stop
+    motor_a.throttle = None
 
-    if (timeStop < time.monotonic()):
-        #print(('timesup',))
-        motor_a.throttle = None
-    print((button_a.value , switch.value, timeStop, time.monotonic(), motor_a.throttle))
- 
-    # print(time.monotonic())
+# If timeStop has elapsed
+if (timeStop < time.monotonic()):
+    # Brake to a stop
+    motor_a.throttle = None
 
-    
-    # use conditional variables to determine which "state" you want to be in
-    # if button is hit
-    #  tStop  = current time + 5s
-    #  goStop = 1
-    #
-    # if switch.val == 0
-    #   goStop = 0
-    #
-    # if tStop < current time
-    #   goStop = 0
-    #
-    # if goStop = 1
-    #   motor throttle = 1
-    # else
-    #   motor throttle = 0
-#Mason Divers
+# Print button and switch values, timeStop, current time, and motor throttle
+print((button_a.value, switch.value, timeStop, time.monotonic(), motor_a.throttle))
+
+# Use conditional variables to determine which "state" you want to be in
+# If button is hit
+#  tStop  = current time + 5s
+#  goStop = 1
+#
+# If switch.val == 0
+#   goStop = 0
+#
+# If tStop < current time
+#   goStop = 0
+#
+# If goStop = 1
+#   motor throttle = 1
+# Else
+#   motor throttle = 0

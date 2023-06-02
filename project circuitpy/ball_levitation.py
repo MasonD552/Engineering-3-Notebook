ee09e3e4c715f7592c2cddd54d8200dbcc7aebfb
MasonD552 <mdivers67@charlottesvilleschools.org>
2023-06-02 09:53:14 -0400

    Pid ball levitation

--- project circuitpy/ball_levitation.py
+++ project circuitpy/ball_levitation.py
@@ -4,25 +4,22 @@ from PID_CPY import PID
 import pwmio   
 import time 
 
-pid = PID(100,800,1000)
-pid.setpoint = 35
-pid.output_limits = (24000,34000)
+pid = PID(24000,10,8500)
+pid.setpoint = 14
+pid.output_limits = (20000.00,50000.00)
 
-fanMotor = pwmio.PWMOut(board.D8,duty_cycle = 65535,frequency=5000) # fanfanMotor
+fanMotor = pwmio.PWMOut(board.D8,duty_cycle = 65535) # fanfanMotor
 fanMotor.duty_cycle = 0
 
 dist = adafruit_hcsr04.HCSR04(trigger_pin = board.D3, echo_pin = board.D2)
 
 while True:
     try:
-        height = 20 - dist.distance
+        height = 26 - dist.distance
         speed = int(pid(height))
         fanMotor.duty_cycle = speed
-        print("speed")
-        print(speed)
-        print("height")
-        print(height)
-        print(" ")
+        print("speed ", speed, " height ", height,)
     except RuntimeError:
         print("retry")
     time.sleep(.1)
+#TIP 120 ask deirolf
